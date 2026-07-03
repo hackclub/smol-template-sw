26d8b545b104c7291c0ea01fb0fc69cc61bb7a07
Arcade Wise <l3gacy.b3ta@gmail.com>
2026-07-02 23:22:53 -0400

    Fix form embedding

--- assets/smol.js
+++ assets/smol.js
@@ -1,9 +1,10 @@
 const submitButton = document.getElementById("submit-button");
 
+// This submits to the Smol Unified DB. If you change it, you won't be able to see your submissions in the Smol dashboard.
+// That means you don't get rewards, I'm sorry, talk to me if you need something else. - @Arcade W.
 if (submitButton) {
     fetch('/smol.json').then(r => r.json()).then(({ project }) => {
-        // set the fillout button's hidden field to `project`
-        submitButton.dataset.program_slack_channel = project;
+        submitButton.href = `https://forms.hackclub.com/t/pu3opxJHjDus?program_slack_channel=${project}`;
     })
 } else {
     alert("Submit button not found. Please check the ID of the submit button.");
